36c0496a04df2b0c4842140c252b68b5ec752103
NourHenni <nourbenhenni687@gmail.com>
2024-12-08 18:03:54 +0100

    pfa controller and routes updated

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import userRoutes from "./routes/UserRoute.js";
 import pfaRoutes from "./routes/pfaRoute.js";
 
 import { seedDatabase } from "./config/seed.js";
+import { authMiddleware } from "./middellwares/authMiddellware.js";
 
 const app = express(); // Create the Express application
 
@@ -18,7 +19,7 @@ app.use(express.json()); // Enable middleware for parsing JSON
 
 // Routes
 app.use("/api", userRoutes);
-app.use("/api/pfa", pfaRoutes);
+app.use("/api/pfa", authMiddleware, pfaRoutes);
 
 // Function to connect database, seed, and start the server
 const startServer = async () => {
