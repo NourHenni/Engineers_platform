750898bc714148d109729c52bd4aa1a45c33ac38
Your Name <you@example.com>
2024-12-10 23:28:52 +0100

    fix changes

--- controllers/matiereController.js
+++ controllers/matiereController.js
@@ -126,5 +126,5 @@ export const getMatieresByEnseignant = async (req, res) => {
 };
 export default { createMatiere, getMatieres, getMatieresByEnseignant };
 
-};
+
 
--- routes/matiereRoute.js
+++ routes/matiereRoute.js
@@ -1,25 +1,35 @@
 import express from "express";
-import { createMatiere, getMatieres, publishOrHideMatieres} from "../controllers/matiereController.js";
+import {
+  createMatiere,
+  getMatieres,
+  publishOrHideMatieres,
+  getMatieresByEnseignant,
+} from "../controllers/matiereController.js";
 import { authMiddleware } from "../middellwares/authMiddellware.js";
-import { isAdmin, isEnseignantOrEtudiant } from "../middellwares/roleMiddellware.js";
+import {
+  isAdmin,
+  isEnseignantOrEtudiant,
+} from "../middellwares/roleMiddellware.js";
 const router = express.Router();
-
 
 // Routes CRUD
 
-router.post("/", matiereController.createMatiere);
-router.get("/", matiereController.getMatieres);
-router.get(
-  "/enseignants/:enseignantId/matieres",
-  matiereController.getMatieresByEnseignant
-);
-
 router.post("/", createMatiere);
-router.get("/getmatieres",authMiddleware,isAdmin, getMatieres);
-router.get("/getmatieres/:id",authMiddleware,isEnseignantOrEtudiant, getMatieres);
-router.post("/publish/:response",authMiddleware, isAdmin,publishOrHideMatieres);
+router.get("/", getMatieres);
+router.get("/enseignants/:enseignantId/matieres", getMatieresByEnseignant);
 
-
-
+router.get("/getmatieres", authMiddleware, isAdmin, getMatieres);
+router.get(
+  "/getmatieres/:id",
+  authMiddleware,
+  isEnseignantOrEtudiant,
+  getMatieres
+);
+router.post(
+  "/publish/:response",
+  authMiddleware,
+  isAdmin,
+  publishOrHideMatieres
+);
 
 export default router;
