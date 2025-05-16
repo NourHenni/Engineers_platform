6ca2cdcdaff81b60929ad3dd6758e8ed87089fbc
redddFF <amdouniwajdi98@gmail.com>
2025-05-16 02:49:51 +0100

    fixed user routes and year controller

---
+++ controllers/appController.js
@@ -0,0 +1,25 @@
+import Year from "../models/yearModel.js";
+
+export async function getLastYear(req, res) {
+  try {
+    const lastYear = await Year.getLastCreatedYear();
+
+    if (!lastYear) {
+      return res.status(404).json({
+        success: false,
+        message: 'No academic years found'
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: lastYear
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Error fetching last academic year',
+      error: error.message
+    });
+  }
+}
